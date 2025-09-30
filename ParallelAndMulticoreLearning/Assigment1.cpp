647f95fbbf3cd7c46704568aaa5301fd9359e404
aidanhallEE <spacetortoise1@outlook.com>
2025-09-30 13:49:49 -0400

    First Commit of code

---
+++ ParallelAndMulticoreLearning/Assigment1.cpp
@@ -0,0 +1,126 @@
+#include <iostream> //output
+#include <fstream> // write to file
+#include <chrono> //time
+#include <atomic> //threads
+#include <thread> //Need this for threads i think?
+#include <math.h> // Sqrt
+#include <tuple> // For multi output
+#include <iomanip>
+#include <vector> // i dont even know anymore
+#include <algorithm> // Needed for std::sort and std::nth_element
+#include <mutex>     // Needed for thread-safe access to the primes list
+
+using namespace std;
+std::vector<std::thread> threads;
+
+// Declare Variables
+int rangeInput;
+int threadInput;
+
+atomic <int> valueToCheck = 2;
+
+atomic<int> finalTotal = 0;
+atomic<long long int> finalSum = 0;
+vector <int> finalArray; // Placeholder for array
+float finalTime;
+
+// File
+ofstream File("Output.txt", ios::app);
+
+// Declare Functions
+
+void ResultsToFile(float time, int total, int sum, int array[]);
+void ThreadSorter(int n, int threadNum);
+void PrimeFinder(int n) {
+	int sen = 0;
+	long long int threadSum = 0;
+	int threadTotal = 0;
+	vector <int> array;
+
+	while(1){
+		int i = valueToCheck.fetch_add(1);
+
+		if (i > n) {
+			break; // All numbers checked
+		}
+
+
+		for (int j = 2; j < sqrt(i); j++) { //Sqrt of i reduces time
+			if (i % j == 0) {
+				sen = 1;
+				break;
+			}
+		}
+		if (sen == 0) {
+			threadTotal++;
+			threadSum = threadSum + i;
+			array.push_back(i);
+			//cout << i << ", ";
+		}
+		else {
+			sen = 0;
+		}
+	}
+	// Output
+	finalTotal.fetch_add(threadTotal);
+	finalSum.fetch_add(threadSum);
+	//Append array to final array
+}
+
+int main() {
+	// Input
+	cout << "Enter Range: ";
+	cin >> rangeInput;
+
+	cout << "Enter # of Threads: ";
+	cin >> threadInput;
+
+	// DO IT
+	// Start Clock
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+	// Do function
+	ThreadSorter(rangeInput, threadInput);
+
+	// End clock
+	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+	//Finalize Data
+	finalTime = (std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count()/(float)1000);
+
+	//int cutdownArray[finalArray.size()]
+	//std::copy(v.begin(), v.end(), cutdownArray);
+	// Print results
+	ResultsToFile(finalTime, finalTotal, finalSum, cutdownArray);
+	// Fix Array or we are fucked!
+
+	// Makes VS happy
+	return 1;
+}
+
+
+// Define the other functions
+//======================================================
+
+void ResultsToFile(float time, int total, int sum, int array[]) {
+	//<execution time> <total number of primes found> <sum of all primes found> <top ten maximum primes, listed in order from lowest to highest>
+	File << "Inputs: (range: " << rangeInput << " | threads: " << threadInput << ") Outputs: " << fixed << setprecision(3) << time << "s | Total #: " << total << " | Sum: " << sum << " | " << array << "\n";
+	File.close();
+}
+
+
+void ThreadSorter(int n, int threadNum) {
+	//Clear it out
+	threads.clear();
+
+	for (int t = 0; t < threadNum; t++) {
+		// Every thread runs the same function, racing for the next number
+		threads.emplace_back(std::thread(PrimeFinder, n));
+	}
+
+	// Join the threads
+	for (int i = 0; i < threadNum; i++)
+	{
+		threads[i].join();
+	}
+}
