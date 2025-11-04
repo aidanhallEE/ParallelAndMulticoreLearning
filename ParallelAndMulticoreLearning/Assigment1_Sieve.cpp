5cd0172f0260d7b04a694ca227a8baf9344a35c2
aidanhallEE <spacetortoise1@outlook.com>
2025-11-03 20:26:20 -0500

    Finished basic prime sieve

--- ParallelAndMulticoreLearning/Assigment1_Sieve.cpp
+++ ParallelAndMulticoreLearning/Assigment1_Sieve.cpp
@@ -1,4 +1,32 @@
 #include <iostream>
+#include <math.h> // For: Sqrt
+#include <vector>    // For: Arrays but better
+#include <thread>
+#include <fstream>   // For: File operations
+#include <mutex>
+
+// Simplification
+using namespace std;
+std::vector<std::thread> threads;
+
+// Declare Variables
+int rangeInput;
+int threadInput;
+
+// Starting Value
+vector <bool> compositeNumArray;
+
+atomic <int> valueToCheck = 2;
+
+atomic<int> finalTotal = 0;
+atomic<unsigned long long> finalSum = 0;
+float finalTime;
+
+vector <int> finalArray;
+mutex finalArray_mutex;
+
+// File
+ofstream File("primes.txt", ios::app);
 
 // Prototypes
 bool isPrime(int number);
@@ -10,21 +38,35 @@ int main() {
 
 	// Prompts
 	std::cout << "n: ";
-	std::cin >> nInput;
+	std::cin >> rangeInput;
 
-	// test output
-	std::cout << isPrime(nInput);
+	//array
+	compositeNumArray.resize(rangeInput+1);
 
-	// Make array size of input
 	// Going Up to sqrt(n), check each number that hasn't been marked prime yet and update all it's multiples
 
-	for (int i = 2; i < nInput; i++) {
-		//if (isPrime(nInput)){
+	for (int i = 2; i < sqrt(rangeInput); i++) {
+		// Add another if statement containing the rest checking if the array[i] is 0 <- This makes sure we don't do unneeded checks
+		if (isPrime(i) && compositeNumArray[i] == 0) {
 		// Update allllll of the multiples of the number up to n
-		// }
+			for (int j = i; i*j < (rangeInput + 1); j++) {
+				std::cout << "do multiple of " << i << ": " << i*j << '\n'; //Debug Print
+				compositeNumArray[i * j] = 1; // Actual thing to use
+			}
+		}
 	}
 
-	// Probably will have to end up making a new
+	for (int i = 2; i < rangeInput+1; i++) {
+		//std::cout << compositeNumArray[i] << "\n\n";
+
+		if (compositeNumArray[i] == 0) {
+			std::cout << i << " ";
+		}
+
+	}
+
+	// Probably will have to end up making a new print!!!!
+
 
 
 	return -1;
